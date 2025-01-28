00ffe837917a5790746d4d688b4a4e8cf370cf7c
Filipp Makarov <fmc@Filipps-MacBook-Pro.local>
2025-01-28 12:26:08 +0300

    codehash

--- test/mock/MockAccount.sol
+++ test/mock/MockAccount.sol
@@ -19,12 +19,12 @@ contract MockAccount is IAccount {
         bytes32 pmCodeHash;
         assembly {
             pmCodeHash := extcodehash(pm)
-            if iszero(eq(pmCodeHash, 0x2c9740fb1b5995a966b700aafdc1da8297f2f60a2f12ce6aeb77d9856e532be1)) {
+            if iszero(eq(pmCodeHash, 0x19e712abe2f3a5ec82e6d5b679f3925c981c9ec06b3fce5a777630bb751098af)) {
                 vd := 0x01 // validation failed
             }
         }
         // else vd remains 0x00 (validation passed)
-       // console2.logBytes32(pmCodeHash);
+       //console2.logBytes32(pmCodeHash);
     }
 
     function execute(address to, uint256 value, bytes calldata data) external returns (bool success, bytes memory result) {
