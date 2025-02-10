4e4194bea3eb61e64e6d228e33a5b501b9996ace
Filipp Makarov <fmc@Filipps-MacBook-Pro.local>
2025-02-10 11:55:51 +0300

    rm adjust V for permit

--- test/unit/K1MEEValidator.t.sol
+++ test/unit/K1MEEValidator.t.sol
@@ -48,9 +48,8 @@ contract K1MEEValidatorTest is BaseTest {
     }
 
     // test simple mode
-    function test_superTxFlow_simple_mode_ValidateUserOp_success(/*uint256 numOfClones*/) public returns (PackedUserOperation[] memory) {
-        //numOfClones = bound(numOfClones, 1, 25);
-        uint256 numOfClones = 8;
+    function test_superTxFlow_simple_mode_ValidateUserOp_success(uint256 numOfClones) public returns (PackedUserOperation[] memory) {
+        numOfClones = bound(numOfClones, 1, 25);
 
         uint256 counterBefore = mockTarget.counter();
         bytes memory innerCallData = abi.encodeWithSelector(MockTarget.incrementCounter.selector);
