f6996ba6fd4762e8a66d362911bee262d51ea165
Filipp Makarov <fmc@Filipps-MacBook-Pro.local>
2025-02-25 18:32:42 +0300

    test fetchers

--- contracts/composability/ComposableExecutionLib.sol
+++ contracts/composability/ComposableExecutionLib.sol
@@ -3,17 +3,17 @@ pragma solidity ^0.8.27;
 
 import "./Storage.sol";
 
+import {console2} from "forge-std/console2.sol";
+
 // Parameter type for composition
 enum InputParamFetcherType {
     RAW_BYTES, // Already encoded bytes
-    STORAGE_READ, // Read from storage
     STATIC_CALL // Perform a static call
 
 }
 
 enum OutputParamFetcherType {
     EXEC_RESULT,
-    STORAGE_READ,
     STATIC_CALL
 }
 
@@ -63,37 +63,20 @@ library ComposableExecutionLib {
         }
         return composedCalldata;
     }
-    
+
+    // TODO: change all abi.decodes to calldata slicing
     function processInput(InputParam calldata param) internal view returns (bytes memory) {
         if (param.fetcherType == InputParamFetcherType.RAW_BYTES) {
             return param.paramData;
+        } else if (param.fetcherType == InputParamFetcherType.STATIC_CALL) {
+            (address contractAddr, bytes memory callData) = abi.decode(param.paramData, (address, bytes));
+            (bool success, bytes memory returnData) = contractAddr.staticcall(callData);
+            if (!success) {
+                revert ExecutionFailed();
+            }
+            return returnData;
         } else {
-            bytes memory rawValue;
-            if (param.fetcherType == InputParamFetcherType.STORAGE_READ) {
-                (address storageContract, bytes32 storageSlot) = abi.decode(param.paramData, (address, bytes32));
-                rawValue = abi.encodePacked(_getValueAt(storageContract, storageSlot));
-            } else if (param.fetcherType == InputParamFetcherType.STATIC_CALL) {
-                (address contractAddr, bytes memory callData) = abi.decode(param.paramData, (address, bytes));
-                (bool success, bytes memory returnData) = contractAddr.staticcall(callData);
-                if (!success) {
-                    revert ExecutionFailed();
-                }
-                rawValue = returnData;
-            } else {
-                revert InvalidParameterEncoding();
-            }
-
-            if (param.valueType == ParamValueType.UINT256) {
-                return abi.encodePacked(uint256(bytes32(rawValue)));
-            } else if (param.valueType == ParamValueType.ADDRESS) {
-                return abi.encodePacked(address(uint160(uint256(bytes32(rawValue)))));
-            } else if (param.valueType == ParamValueType.BYTES32) {
-                return rawValue;
-            } else if (param.valueType == ParamValueType.BOOL) {
-                return abi.encodePacked(uint256(bytes32(rawValue)) != 0);
-            } else {
-                revert InvalidParameterEncoding();
-            }
+            revert InvalidParameterEncoding();
         }
     }
 
@@ -108,23 +91,13 @@ library ComposableExecutionLib {
         if (param.fetcherType == OutputParamFetcherType.EXEC_RESULT) {
             (address targetStorageContract, bytes32 targetSlot) = abi.decode(param.paramData, (address, bytes32));
             Storage(targetStorageContract).writeStorage(targetSlot, abi.decode(returnData, (bytes32)), account);
-        } else if (param.fetcherType == OutputParamFetcherType.STORAGE_READ) {
-            (
-                address sourceStorageContract,
-                bytes32 sourceStorageSlot,
-                address targetStorageContract,
-                bytes32 targetStorageSlot
-            ) = abi.decode(param.paramData, (address, bytes32, address, bytes32));
-            Storage(targetStorageContract).writeStorage(
-                targetStorageSlot, _getValueAt(sourceStorageContract, sourceStorageSlot), account
-            );
         } else if (param.fetcherType == OutputParamFetcherType.STATIC_CALL) {
             (
-                address targetStorageContract,
-                bytes32 targetStorageSlot,
                 address sourceContract,
-                bytes memory sourceCallData
-            ) = abi.decode(param.paramData, (address, bytes32, address, bytes));
+                bytes memory sourceCallData,
+                address targetStorageContract,
+                bytes32 targetStorageSlot
+            ) = abi.decode(param.paramData, (address, bytes, address, bytes32));
             (bool outputSuccess, bytes memory outputReturnData) = sourceContract.staticcall(sourceCallData);
             if (!outputSuccess) {
                 // TODO : USE OTHER ERROR
@@ -134,18 +107,6 @@ library ComposableExecutionLib {
         } else {
             revert InvalidOutputParamFetcherType();
         }
-    }
-
-    function _getValueAt(address contractAddr, bytes32 slot) private view returns (bytes32) {
-        bytes32 value;
-        assembly {
-            // Switch context to read storage from contractAddr
-            let success := staticcall(gas(), contractAddr, 0x00, 0x00, 0x00, 0x00)
-            if iszero(success) { revert(0, 0) }
-            // Read storage slot from the target contract
-            value := sload(slot)
-        }
-        return value;
     }
 
 }
