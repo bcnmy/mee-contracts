3dc95d6eaa81c6b915813ea3715bfb14cf5723c0
Filipp Makarov <fmc@Filipps-MacBook-Pro.local>
2025-03-24 21:13:08 +0300

    add script to deploy just k1mee

---
+++ scripts/bash-deploy/deploy-k1.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+
+### VERIFY INPUTS ###
+printMan() {
+    printf "Usage: $0 <Environment: local|mainnet|testnet> <Network Name>\n"
+}
+
+if [ $# -eq 0 ]; then
+    printf "Please provide private key, environment and network name\n"
+    printMan
+    exit 1
+fi
+
+if [ -z $1 ]; then
+    printf "Please provide environment\n"
+    printMan
+    exit 1
+fi
+
+ENVIRONMENT=$1
+VERIFY=""
+
+if [ $ENVIRONMENT = "local" ]; then
+    CHAIN_NAME="localhost"
+else 
+    if [ $ENVIRONMENT = "mainnet" ] || [ $ENVIRONMENT = "testnet" ]; then
+        if [ -z $2 ]; then
+            printf "Please provide network name\n"
+            printMan
+            exit 1
+        fi
+        CHAIN_NAME=$2
+        VERIFY="--verify"
+    else 
+        printf "Invalid environment\n"
+        printMan
+        exit 1
+    fi
+fi
+
+source ../../.env
+
+# set private key based on the environment
+if [ $ENVIRONMENT = "mainnet" ]; then
+    PRIVATE_KEY=$MAINNET_DEPLOYER_PRIVATE_KEY
+else 
+    if [ $ENVIRONMENT = "testnet" ]; then
+        PRIVATE_KEY=$TESTNET_DEPLOYER_PRIVATE_KEY
+    else 
+        PRIVATE_KEY=$LOCAL_DEPLOYER_PRIVATE_KEY
+    fi
+fi
+
+### DEPLOY PRE-REQUISITES ###
+{ (bash deploy-prerequisites.sh $PRIVATE_KEY $ENVIRONMENT $CHAIN_NAME) } || {
+    printf "Deployment prerequisites failed\n"
+    exit 1
+}
+
+### COPY ARTIFACTS ###
+read -r -p "Do you want to rebuild MEE artifacts from your local sources? (y/n): " proceed
+if [ $proceed = "y" ]; then
+    ### BUILD ARTIFACTS ###
+    printf "Building MEE artifacts\n"
+    { (forge build 1> ./logs/forge-build.log 2> ./logs/forge-build-errors.log) } || {
+        printf "Build failed\n See logs for more details\n"
+        exit 1
+    }
+    printf "Copying MEE artifacts\n"
+    
+    mkdir -p ./artifacts/K1MeeValidator
+    mkdir -p ./artifacts/EtherForwarder
+    
+    cp ../../out/K1MeeValidator.sol/K1MeeValidator.json ./artifacts/K1MeeValidator/.
+    cp ../../out/Forwarder.sol/EtherForwarder.json ./artifacts/EtherForwarder/.
+
+    printf "Artifacts copied\n"
+
+    ### CREATE VERIFICATION ARTIFACTS ###
+    printf "Creating verification artifacts\n"
+    forge verify-contract --show-standard-json-input $(cast address-zero) K1MeeValidator > ./artifacts/K1MeeValidator/verify.json    
+    forge verify-contract --show-standard-json-input $(cast address-zero) EtherForwarder > ./artifacts/EtherForwarder/verify.json
+else 
+    printf "Using precompiled artifacts\n"
+fi
+
+### DEPLOY MEE SCs ###
+printf "Addresses for MEE SCs:\n"
+mkdir -p ./logs/$CHAIN_NAME
+forge script DeployK1 true --sig "run(bool)" --rpc-url $CHAIN_NAME -vv > ./logs/$CHAIN_NAME/$CHAIN_NAME-predeploy-mee.log
+cat ./logs/$CHAIN_NAME/$CHAIN_NAME-predeploy-mee.log | grep -e "Addr" -e "already deployed"
+printf "Do you want to proceed with the addresses above? (y/n): "
+read -r proceed
+if [ $proceed = "y" ]; then
+    printf "Do you want to specify gas price? (y/n): "
+    read -r proceed
+    if [ $proceed = "y" ]; then
+        printf "Enter gas prices args: \n For the EIP-1559 chains, enter two args: base fee and priority fee in gwei\n For the legacy chains, enter one argument. \n Example eip-1559: 20 1 \n Example legacy: 20 \n"
+        read -r -a GAS_ARGS
+        if [ ${#GAS_ARGS[@]} -eq 2 ]; then
+            GAS_SUFFIX="--with-gas-price ${GAS_ARGS[0]}gwei --priority-gas-price ${GAS_ARGS[1]}gwei"
+        else 
+            GAS_SUFFIX="--with-gas-price ${GAS_ARGS[0]}gwei"
+        fi
+    else 
+        GAS_SUFFIX=""
+    fi
+    {   
+        printf "Proceeding with deployment \n"
+        mkdir -p ./logs/$CHAIN_NAME
+        forge script DeployK1 false --sig "run(bool)" --rpc-url $CHAIN_NAME --etherscan-api-key $CHAIN_NAME --private-key $PRIVATE_KEY $VERIFY -vv --broadcast --slow $GAS_SUFFIX 1> ./logs/$CHAIN_NAME/$CHAIN_NAME-deploy-mee.log 2> ./logs/$CHAIN_NAME/$CHAIN_NAME-deploy-mee-errors.log 
+    } || {
+        printf "Deployment failed\n See logs for more details\n====================================\n"
+        exit 1
+    }
+    printf "Deployment successful\n"
+    cat ./logs/$CHAIN_NAME/$CHAIN_NAME-deploy-mee.log | grep -e "deployed at" -e "registered on registry"
+    printf "====================================\n"
+else 
+    printf "Exiting\n"
+    exit 1
+fi  
---
+++ scripts/foundry/DeployK1Mee.sol
@@ -0,0 +1,255 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console2} from "lib/forge-std/src/Script.sol";
+import {K1MeeValidator} from "contracts/validators/K1MeeValidator.sol";
+import {DeterministicDeployerLib} from "./utils/DeterministicDeployerLib.sol";
+import {NodePaymaster} from "contracts/NodePaymaster.sol";
+
+contract DeployK1 is Script {
+
+    address constant ENTRY_POINT_V07 = 0x0000000071727De22E5E9d8BAf0edAc6f37da032;
+    address constant MODULE_REGISTRY_ADDRESS = 0x000000000069E2a187AEFFb852bF3cCdC95151B2;
+
+    address constant ATTESTER_ADDRESS = 0xF9ff902Cdde729b47A4cDB55EF16DF3683a04EAB; // Biconomy Attester
+
+    bytes32 constant MEE_K1_VALIDATOR_SALT = 0x000000000000000000000000000000000000000047819504fd5006001ee95238; //=> 0x00000000d12897DDAdC2044614A9677B191A2d95000000000000000071f7e488ac1c920333dec4c8; // 
+    bytes32 constant ETH_FORWARDER_SALT = 0x00000000000000000000000000000000000000005de583e34ca87b011d9cf2dc; //=> 0x00000000d1AafD53453074C79C9280283B99d17C; // 
+
+    ModuleType[] moduleTypesToAttest;
+
+    function setUp() public {
+     
+    }
+
+    function run(bool check) external {
+
+        if (check) {
+            _checkMEEAddresses();
+        } else {
+            _deployMEE();
+        }
+
+    }
+
+    function _checkMEEAddresses() internal {
+
+        // =================== K1 MEE Validator ===================
+        bytes memory bytecode = vm.getCode("scripts/bash-deploy/artifacts/K1MeeValidator/K1MeeValidator.json");
+
+        address meeK1Validator = DeterministicDeployerLib.computeAddress(bytecode, MEE_K1_VALIDATOR_SALT);
+
+        uint256 codeSize;
+        assembly {
+            codeSize := extcodesize(meeK1Validator)
+        }
+        
+        console2.log("MEE K1 Validator Addr: ", meeK1Validator, " || >> Code Size: ", codeSize);
+
+        console2.log("MEE K1 Validator initcode for salt generation: ");
+        console2.logBytes32(keccak256(bytecode));
+
+        // =================== ETH Forwarder contract ===================
+        bytecode = vm.getCode("scripts/bash-deploy/artifacts/EtherForwarder/EtherForwarder.json");
+        address expectedEtherForwarder = DeterministicDeployerLib.computeAddress(bytecode, ETH_FORWARDER_SALT);
+        assembly {
+            codeSize := extcodesize(expectedEtherForwarder)
+        }
+        console2.log("ETH Forwarder Addr: ", expectedEtherForwarder, " || >> Code Size: ", codeSize);
+
+        console2.log("ETH Forwarder initcode for salt generation: ");
+        console2.logBytes32(keccak256(bytecode));
+
+    }
+
+    function _deployMEE() internal {
+
+        uint256 codeSize;
+
+        // K1 MEE Validator
+        bytes memory bytecode = vm.getCode("scripts/bash-deploy/artifacts/K1MeeValidator/K1MeeValidator.json");
+        address expectedMEEK1Validator = DeterministicDeployerLib.computeAddress(bytecode, MEE_K1_VALIDATOR_SALT);
+        assembly {
+            codeSize := extcodesize(expectedMEEK1Validator)
+        }
+        if (codeSize > 0) {
+            console2.log("MEE K1 Validator already deployed at: ", expectedMEEK1Validator, " skipping deployment");
+        } else {
+            address meeK1Validator = DeterministicDeployerLib.broadcastDeploy(bytecode, MEE_K1_VALIDATOR_SALT);
+            console2.log("MEE K1 Validator deployed at: ", meeK1Validator);
+        }
+        
+        if (registerModule(expectedMEEK1Validator)) {
+            attestModule(expectedMEEK1Validator);
+        }
+
+        // ETH Forwarder contract
+        bytecode = vm.getCode("scripts/bash-deploy/artifacts/EtherForwarder/EtherForwarder.json");
+        address expectedEtherForwarder = DeterministicDeployerLib.computeAddress(bytecode, ETH_FORWARDER_SALT);
+        assembly {
+            codeSize := extcodesize(expectedEtherForwarder)
+        }
+        if (codeSize > 0) {
+            console2.log("ETH Forwarder already deployed at: ", expectedEtherForwarder, " skipping deployment");
+        } else {
+            address etherForwarder = DeterministicDeployerLib.broadcastDeploy(bytecode, ETH_FORWARDER_SALT);
+            console2.log("ETH Forwarder deployed at: ", etherForwarder);
+        }
+
+    }
+
+    function registerModule(address moduleAddress) internal returns (bool) {
+        IRegistryModuleManager registry = IRegistryModuleManager(MODULE_REGISTRY_ADDRESS);
+
+        uint256 codeSize;
+        assembly {
+            codeSize := extcodesize(MODULE_REGISTRY_ADDRESS)
+        }
+        if (codeSize == 0) {
+            console2.log("Module registry not deployed => module not registered on registry");
+            return false;
+        }
+        ResolverUID resolverUID = ResolverUID.wrap(0xdbca873b13c783c0c9c6ddfc4280e505580bf6cc3dac83f8a0f7b44acaafca4f);
+        ModuleRecord memory moduleRecord = registry.findModule(moduleAddress);
+
+        bool isRegistered = ResolverUID.unwrap(moduleRecord.resolverUID) != bytes32(0x0000000000000000000000000000000000000000000000000000000000000000);
+        bool res;
+        if (isRegistered) {
+            console2.log("Module already registered on registry");
+            return true;
+        } else {
+            vm.startBroadcast();
+            try registry.registerModule(
+                resolverUID,
+                moduleAddress,
+                hex"",
+                hex""
+            ) {
+                console2.log("Module registered on registry");
+                res = true;
+            } catch (bytes memory reason) {
+                console2.log("Module not registered on registry: registration failed");
+                console2.logBytes(reason);
+                res = false;
+            }
+            vm.stopBroadcast();
+        }
+        return res;
+    }
+
+    function attestModule(address moduleAddress) internal returns (bool) {
+        IRegistryModuleManager registry = IRegistryModuleManager(MODULE_REGISTRY_ADDRESS);
+
+        address[] memory attesters = new address[](1);
+        attesters[0] = ATTESTER_ADDRESS;
+        
+        ModuleType[] memory moduleTypes = new ModuleType[](1);
+        moduleTypes[0] = ModuleType.wrap(uint256(1)); // validator
+
+        // check if module is already attested
+        uint256 needToAttest = 0;
+        for (uint256 i; i < moduleTypes.length; i++) {
+            ModuleType moduleType = moduleTypes[i];
+            try registry.check(moduleAddress, moduleType, attesters, 1) {
+                console2.log("Attestation as type %s successful, check passed", ModuleType.unwrap(moduleType));
+            } catch (bytes memory reason) {
+                console2.log("Module not attested as type %s, attesting...", ModuleType.unwrap(moduleType));
+                needToAttest++;
+                moduleTypesToAttest.push(moduleType);
+            }
+        }
+
+        if (needToAttest == 0) {
+            console2.log("Module already attested, skipping attestation");
+            return true;
+        }
+
+        if (moduleTypesToAttest.length != needToAttest) {
+            revert("Module types to attest mismatch");
+        }
+
+        AttestationRequest memory meeK1ValidatorAttestationRequest = AttestationRequest({
+            moduleAddress: moduleAddress,
+            expirationTime: uint48(block.timestamp + 3650 days),
+            data: bytes(""),
+            moduleTypes: moduleTypesToAttest
+        });
+
+        bytes memory cd = abi.encodeWithSelector(
+            // attest(bytes32, AttestationRequest) (0x945e3641) 
+            bytes4(0x945e3641),
+            bytes32(0x93d46fcca4ef7d66a413c7bde08bb1ff14bacbd04c4069bb24cd7c21729d7bf1), //schema UID <= need to be added by Rhinestone to the registry
+            meeK1ValidatorAttestationRequest
+        );
+        //console.logBytes(cd);
+
+        vm.startBroadcast();
+
+        IAttester attester = IAttester(ATTESTER_ADDRESS);
+
+        try attester.adminExecute(Execution({
+            target: MODULE_REGISTRY_ADDRESS,
+            value: 0,
+            callData: cd
+        })) {
+            console2.log("Attestation successful, re-checking");
+            for (uint256 i; i < moduleTypesToAttest.length; i++) {
+                ModuleType moduleType = moduleTypesToAttest[i];
+                console2.log("Checking attestations for module %s with type %s", moduleAddress, ModuleType.unwrap(moduleType));
+                try registry.check(moduleAddress, moduleType, attesters, 1) {
+                    console2.log("Attestation successful, check passed");
+                } catch (bytes memory reason) {
+                    console2.log("Check failed");
+                    console2.logBytes(reason);
+                }
+            }
+        } catch (bytes memory reason) {
+            console2.log("Attestation failed");
+            console2.logBytes(reason);
+        }
+
+        vm.stopBroadcast();
+    }
+}
+
+/// ================================
+
+type ResolverUID is bytes32;
+
+struct ModuleRecord {
+    ResolverUID resolverUID; // The unique identifier of the resolver.
+    address sender; // The address of the sender who deployed the contract
+    bytes metadata; // Additional data related to the contract deployment
+}
+
+struct Execution {
+    address target;
+    uint256 value;
+    bytes callData;
+}
+
+type ModuleType is uint256;
+
+struct AttestationRequest {
+    address moduleAddress;
+    uint48 expirationTime;
+    bytes data;
+    ModuleType[] moduleTypes;
+}
+
+interface IRegistryModuleManager {
+    function registerModule(
+        ResolverUID resolverUID,
+        address moduleAddress,
+        bytes calldata metadata,
+        bytes calldata resolverContext
+    ) external;
+
+    function findModule(address moduleAddress) external view returns (ModuleRecord memory);
+
+    function check(address module, ModuleType moduleType, address[] calldata attesters, uint256 threshold) external view;
+}
+
+interface IAttester {
+    function adminExecute(Execution memory execution) external;
+}
