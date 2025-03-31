cb8256bee571891ac3392614a2c3453c2dec88eb
Filipp Makarov <fmc@Filipps-MacBook-Pro.local>
2025-03-31 16:47:55 +0700

    test acces control

---
+++ test/unit/NodePMAccessControl.t.sol
@@ -0,0 +1,102 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import {BaseTest} from "../Base.t.sol";
+import {Vm} from "forge-std/Test.sol";
+import {PackedUserOperation, UserOperationLib} from "account-abstraction/core/UserOperationLib.sol";
+import {MockTarget} from "../mock/MockTarget.sol";
+import {MockAccount} from "../mock/MockAccount.sol";
+import {IEntryPointSimulations} from "account-abstraction/interfaces/IEntryPointSimulations.sol";
+import {EntryPointSimulations} from "account-abstraction/core/EntryPointSimulations.sol";
+import {NodePaymaster} from "contracts/NodePaymaster.sol";
+import {IEntryPoint} from "account-abstraction/interfaces/IEntryPoint.sol";
+import {EmittingNodePaymaster} from "../mock/EmittingNodePaymaster.sol";
+import "contracts/types/Constants.sol";
+
+import "forge-std/console2.sol";
+
+contract PMPerNodeTest is BaseTest {
+    using UserOperationLib for PackedUserOperation;
+
+    Vm.Wallet wallet;
+    MockAccount mockAccount;
+    uint256 constant PREMIUM_CALCULATION_BASE = 100e5;
+    uint256 valueToSet;
+
+    function setUp() public virtual override {
+        super.setUp();
+        mockAccount = deployMockAccount({validator: address(0), handler: address(0)});
+        wallet = createAndFundWallet("wallet", 1 ether);
+        mockTarget.setValue(0);
+    }
+
+    function _prepareUserOps() public returns (PackedUserOperation[] memory) {
+        valueToSet = MEE_NODE_HEX;
+        uint256 premiumPercentage = 17_00000;
+        
+        bytes memory innerCallData = abi.encodeWithSelector(MockTarget.setValue.selector, valueToSet);
+        bytes memory callData =
+            abi.encodeWithSelector(mockAccount.execute.selector, address(mockTarget), uint256(0), innerCallData);
+        PackedUserOperation memory userOp = buildUserOpWithCalldata({
+            account: address(mockAccount),
+            callData: callData,
+            wallet: wallet,
+            preVerificationGasLimit: 50e3,
+            verificationGasLimit: 35e3,
+            callGasLimit: 100e3
+        });
+
+        uint128 pmValidationGasLimit = 25_000;
+        uint128 pmPostOpGasLimit = 20_000; // 12_000 is enough in the wild, here we add for emitting events in the wrapper
+        uint256 maxGasLimit = userOp.preVerificationGas + unpackVerificationGasLimitMemory(userOp)
+            + unpackCallGasLimitMemory(userOp) + pmValidationGasLimit + pmPostOpGasLimit;
+
+        userOp.paymasterAndData = makePMAndDataForOwnPM({
+            nodePM: address(NODE_PAYMASTER),
+            pmValidationGasLimit: pmValidationGasLimit,
+            pmPostOpGasLimit: pmPostOpGasLimit,
+            pmMode: NODE_PM_MODE_USER,
+            premiumMode: NODE_PM_PREMIUM_PERCENT,
+            financialData: premiumPercentage // percentage premium = 17% of maxGasCost
+        });
+        // account owner does not need to re-sign the userOp as mock account does not check the signature
+
+        PackedUserOperation[] memory userOps = new PackedUserOperation[](1);
+        userOps[0] = userOp;
+
+        return (userOps);
+    }
+
+    function test_reverts_if_sent_by_non_approved_EOA() public {
+        PackedUserOperation[] memory userOps = _prepareUserOps();
+
+        userOps[0] = addNodeMasterSig(userOps[0], MEE_NODE, MEE_NODE_EXECUTOR_EOA);
+
+        vm.startPrank(address(0xdeadbeef)); // submitter is not an executor EIA signed above
+
+        vm.expectRevert(abi.encodeWithSignature("FailedOp(uint256,string)", 0, "AA34 signature error"));
+        ENTRYPOINT.handleOps(userOps, payable(MEE_NODE_ADDRESS));
+
+        uint256 mockTargetValueBefore = mockTarget.value();
+        assertFalse(mockTargetValueBefore == valueToSet);
+        assertEq(mockTargetValueBefore, mockTarget.value());
+
+        vm.stopPrank();
+    }
+
+    // test reverts if userOp hash was not signed
+    function test_reverts_if_userOp_hash_was_not_signed() public {
+        PackedUserOperation[] memory userOps = _prepareUserOps();
+        userOps[0] = addNodeMasterSig(userOps[0], MEE_NODE, MEE_NODE_EXECUTOR_EOA); 
+ 
+        userOps[0].preVerificationGas++; // change some data
+        
+        // Do not re-sign with MEE Node EOA, so the userOp hash is not signed by the Node
+
+        vm.startPrank(MEE_NODE_EXECUTOR_EOA, MEE_NODE_EXECUTOR_EOA); // should revert despite of the correct tx.origin
+        vm.expectRevert(abi.encodeWithSignature("FailedOp(uint256,string)", 0, "AA34 signature error"));
+        ENTRYPOINT.handleOps(userOps, payable(MEE_NODE_ADDRESS));
+        vm.stopPrank();
+    }
+
+}
